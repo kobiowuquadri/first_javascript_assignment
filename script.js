e10747adc6f8d7d5f0cedc1aadd3bff270c04ca6
Kobiowu Quadri A <kobiowuq@gmail.com>
2023-01-16 19:50:02 +0100

    my first javascript assignment

---
+++ script.js
@@ -0,0 +1,18 @@
+//This program alert get user age and alert it
+let age = prompt('How old are you?');
+alert(`You are ${age} years old`);
+
+//this program initialise the variables: firstName, lastName, Age
+let firstName, lastName, Age;
+firstName = "John";
+lastName = "Doe";
+Age = 35;
+// console.log(firstName);
+// console.log(lastName);
+// console.log(Age);
+
+//This program alert z which is sum of x and y
+var x = 5;
+var y = 10;
+var z = x + y;
+alert(z);
